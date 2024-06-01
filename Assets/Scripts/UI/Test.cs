79d1bee8f375005f1941c273d0ace6726ddb1320
Sydney Kolehmainen <fsyko001@edu.xamk.fi>
2024-06-01 23:46:18 +0300

    Starting on the player

--- Assets/Scripts/UI/Test.cs
+++ Assets/Scripts/UI/Test.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SLC.Bad4Business.Core
+namespace SLC.Bad4Business.UI
 {
-    public class CharacterMovement : MonoBehaviour
+    public class Test : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
