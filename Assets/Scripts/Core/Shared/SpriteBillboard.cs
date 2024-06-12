d942047145f8510ec9294164e04fbd5db3654a64
Sydney Kolehmainen <fsyko001@edu.xamk.fi>
2024-06-12 16:49:16 +0300

    Assets folder setup + billboarding

---
+++ Assets/Scripts/Core/Shared/SpriteBillboard.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace SLC.Bad4Business.Core
+{
+    public class SpriteBillboard : MonoBehaviour
+    {
+        [SerializeField] private bool alignToCamera = true;
+
+        private Transform m_cameraTransform;
+
+        #region Built-In Methods
+        private void Start()
+        {
+            m_cameraTransform = Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            if (alignToCamera)
+            {
+                transform.rotation = m_cameraTransform.rotation;
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0f, m_cameraTransform.rotation.eulerAngles.y, 0f);
+            }            
+        }
+        #endregion
+    }
+}
